e14defce65c56696d3f7b0b70950124a8449f1ea
RPOOVENDRAPANDI <rpoovendrapandi@outlook.com>
2024-08-16 23:28:28 +0530

    Updated new build

--- Z2H/admin/dist/spa/assets/route.2486d570.js
+++
@@ -1,1 +0,0 @@
-import{f as t}from"./index.18d27cdb.js";import{u as a}from"./axios.621c1308.js";const r=a();var u=t(({router:o})=>{o.beforeEach(async(n,e,s)=>{r.token&&!r.userInfo&&r.getUserInfo(),i(n,e,s)})});const i=(o,n,e)=>{if(!r.token&&o.name!=="sign-in")return e({name:"sign-in"});if(!o.name)return e({name:"sign-in"});e()};export{u as default,i as validateRoutes};
